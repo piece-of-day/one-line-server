f54ffc8294bd5545f9bc6411da472c90062b6a0d
qf9ar8nv <ckguscjf0@gmail.com>
2022-12-17 18:04:21 +0900

    add signout process

--- src/main/kotlin/kr/pieceofday/onelineserver/controller/UserController.kt
+++ src/main/kotlin/kr/pieceofday/onelineserver/controller/UserController.kt
@@ -2,17 +2,26 @@ package kr.pieceofday.onelineserver.controller
 
 import kr.pieceofday.onelineserver.domain.User
 import kr.pieceofday.onelineserver.repository.SessionRepository
+import kr.pieceofday.onelineserver.util.CookieUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
-@Controller
+@RestController
 class UserController(
     val sessionRepository: SessionRepository
 ) {
 
     @GetMapping("/signout")
-    fun signOut(@AuthenticationPrincipal user: User) {
-        TODO("sign out process")
+    fun signOut(@AuthenticationPrincipal user: User, request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<Void> {
+        sessionRepository.remove(user.id.toString())
+        CookieUtils.deleteCookie(request, response, CookieUtils.SESSION_COOKIE_NAME)
+        CookieUtils.deleteCookie(request, response, CookieUtils.OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+        return ResponseEntity.status(HttpStatus.OK).build()
     }
 }
