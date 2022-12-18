3a0e229d343c4b4332833dc089674e6276046ec0
qf9ar8nv <ckguscjf0@gmail.com>
2022-12-18 14:42:22 +0900

    update Line Service Logic except Read Service

--- src/main/kotlin/kr/pieceofday/onelineserver/service/LineServiceImpl.kt
+++ src/main/kotlin/kr/pieceofday/onelineserver/service/LineServiceImpl.kt
@@ -6,24 +6,51 @@ import kr.pieceofday.onelineserver.dto.CreateLineDTO
 import kr.pieceofday.onelineserver.dto.UpdateLineDTO
 import kr.pieceofday.onelineserver.repository.LineRepository
 import org.springframework.stereotype.Service
+import javax.naming.AuthenticationException
 
 @Service
 class LineServiceImpl(
     val lineRepository: LineRepository
 ): LineService {
     override fun createLine(user: User, createLineDto: CreateLineDTO): Line {
-        TODO("Not yet implemented")
+        val line = Line(
+            title = createLineDto.title.name,
+            content = createLineDto.content,
+            liked = 0,
+            reported = 0,
+            user = user
+        )
+        return lineRepository.save(line)
     }
 
     override fun readLine(cnt: Int): List<Line> {
-        TODO("Not yet implemented")
+        TODO("Get Random Line for specified Line")
     }
 
     override fun updateLine(user: User, id: Long, updateLineDto: UpdateLineDTO): Line {
-        TODO("Not yet implemented")
+        val line = getLineOrThrowError(id)
+        checkAuthenticationOrThrowError(user, line)
+
+        line.title = updateLineDto.title.name
+        line.content = updateLineDto.content
+
+        return line
     }
 
     override fun deleteLine(user: User, id: Long) {
-        TODO("Not yet implemented")
+        val line = getLineOrThrowError(id)
+        checkAuthenticationOrThrowError(user, line)
+
+        lineRepository.delete(line)
+    }
+
+    private fun getLineOrThrowError(id: Long): Line {
+        return lineRepository.findById(id).orElseThrow { throw IllegalArgumentException("잘못된 id 값입니다.") }
+    }
+
+    private fun checkAuthenticationOrThrowError(user: User, line: Line) {
+        if (user.id != line.user.id) {
+            throw AuthenticationException("권한이 없습니다.")
+        }
     }
 }
